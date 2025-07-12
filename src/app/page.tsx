86a693508222093b93f76b5790fbc5c5e2c74d81
akashbhardwaj23 <akashbhardwaj415@gmail.com>
2025-07-12 18:32:31 +0530

    added some ui home page logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
 
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+    <main className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-8">
+      <div className="col-span-1 flex justify-center items-center">
+        <h1>Filter logic</h1>
+      </div>
+      <div className="col-span-2">
+        
+          <h1 className="text-4xl font-bold">Product Listing</h1>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div className="col-span-1">
+              <h2>Product 1</h2>
+            </div>
+          </div>
+      </div>
+    </main>
   );
 }
