3495efea6373e9319d7bbfd2e461b9ce875ea9fd
akashbhardwaj23 <akashbhardwaj415@gmail.com>
2025-07-12 13:36:57 +0530

    added navbar code and some colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -9,6 +10,12 @@ const geistSans = Geist({
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
@@ -25,8 +32,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${inter.className} ${geistMono.variable} antialiased`}
       >
+        <Navbar />
         {children}
       </body>
     </html>
---
+++ src/components/navbar.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import { Search, ShoppingCart } from "lucide-react";
+
+export default function Navbar() {
+  return (
+    <div className="flex justify-between items-center px-8 bg-primary font-inter">
+      <div className="flex items-center">
+        <Image src="/logo.png" alt="logo" width={100} height={100} className="w-20 h-20 object-cover" />
+      </div>
+      <div className="flex items-center w-96 relative text-primary-text">
+        <Search className="w-5 h-5 absolute left-4 top-1/2 -translate-y-1/2" />
+        <input type="text" placeholder="Search for products..." className="w-full h-10 rounded-md border border-primary-text p-2 pl-12 focus:outline-none placeholder:text-primary-text" />
+      </div>
+      <div className="flex items-center">
+        <button className="flex items-center gap-2 bg-secondary-background text-secondary-text px-6 py-3 rounded-[8px] text-sm font-semibold">
+            <ShoppingCart className="w-4 h-4" />
+            <span>Cart</span>
+        </button>
+      </div>
+    </div>
+  );
+}
